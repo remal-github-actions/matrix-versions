f835d0866beed16fbc77d6b232f6a3ecc07d9020
Semyon Levin <levin.semen@gmail.com>
2024-08-08 23:04:43 -0700

    Fix build

--- process-dist.js
+++ process-dist.js
@@ -7,8 +7,10 @@ const renovateVersion = pkg.dependencies.renovate ?? 'unknown'
 const dir = path.resolve(__dirname, 'dist')
 fs.chmodSync(dir, 0o777)
 console.log(JSON.stringify(fs.statSync(dir), null, 2))
+console.log('dir', fs.statSync(dir).isDirectory())
 
 const fileNames = fs.readdirSync(dir)
+    .filter(name => name.endsWith('.js'))
 for (const fileName of fileNames) {
     const file = path.resolve(dir, fileName)
     const content = fs.readFileSync(file, 'utf-8')
