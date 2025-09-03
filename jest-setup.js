8ef870bac616b72e6c87ec81e6841e279a55a59d
Semyon Levin <levin.semen@gmail.com>
2025-09-03 15:46:12 +0400

    fix build

--- jest-setup.js
+++ jest-setup.js
@@ -3,4 +3,6 @@ expect.extend(require('jest-extended'))
 require('./src/internal/initRenovateLogging').initRenovateLogging()
 
 const core = require('@actions/core')
-core.error = core.warning
+core.error = (message, properties) => {
+    core.warning(`ERROR: ${message}`, properties)
+}
